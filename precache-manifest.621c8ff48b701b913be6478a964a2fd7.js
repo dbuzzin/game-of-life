462b8643c43186e47793d9580f86a23491e2e435
dbuzzin <d.pearson1991@googlemail.com>
2020-01-09 13:03:32 +0000

    Updates

---
+++ precache-manifest.621c8ff48b701b913be6478a964a2fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757264de70b7053f68915bd66bc17601",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "b5d11e3a22c37bd45a0a",
+    "url": "/game-of-life/static/css/main.09085787.chunk.css"
+  },
+  {
+    "revision": "0eed82864cbd6f00d21e",
+    "url": "/game-of-life/static/js/2.782e821b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-of-life/static/js/2.782e821b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5d11e3a22c37bd45a0a",
+    "url": "/game-of-life/static/js/main.1ee69827.chunk.js"
+  },
+  {
+    "revision": "e9d08bf348adbf3021f8",
+    "url": "/game-of-life/static/js/runtime-main.f914b084.js"
+  }
+]);
